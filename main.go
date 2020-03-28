3dcf8b8104c55e875c3b0f0fc113fa747633317e
eco9999 <emrecanocak@windowslive.com>
2020-03-28 17:52:04 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package errorx
+
+import (
+	"fmt"
+)
+
+// Error is main struct of errorx package
+type Error struct {
+	Header string
+	Info   string
+	Code   int
+	Inner  []error
+}
+
+func New(Header, Info string, Code int) *Error {
+	return &Error{Header: Header, Info: Info, Code: Code}
+}
+
+func (e *Error) Link(err error) *Error {
+	e.Inner = append(e.Inner, err)
+	return e
+}
+
+func (e *Error) Error() string {
+	if e == nil {
+		return ""
+	}
+	lene := len(e.Inner)
+	if lene == 0 {
+		return fmt.Sprintf("%v: %v, Error Code:%03d ", e.Header, e.Info, e.Code)
+	}
+	str := fmt.Sprintf("%v: %v, Error Code:%03d.", e.Header, e.Info, e.Code) + "\n"
+	str += "Linked Errors:\n"
+	for i := 0; i < lene-1; i++ {
+		err := e.Inner[i]
+		str += fmt.Sprintf("\t- %v\t", err.Error())
+	}
+	err := e.Inner[lene-1]
+	str += fmt.Sprintf("\t- %v", err.Error())
+	return str
+}
